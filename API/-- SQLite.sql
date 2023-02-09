d5575014fbd5909ea60151e84311b6cb0e310f18
Jabin Mejía <Jabin.Mejia@gt.ey.com>
2023-02-09 12:25:11 -0600

    first commit

---
+++ API/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (4, "Juan");
+
+INSERT INTO Users (Id, UserName)
+VALUES (5, "Andrés");
+
+INSERT INTO Users (Id, UserName)
+VALUES (6, "Tom");
---
+++ API/Program.cs
@@ -0,0 +1,23 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using API.Controllers;
+using API.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapControllers();
+
+app.Run();
